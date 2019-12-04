d053627591dc99a1a33a39bd4703eeee6658961e
David Brad <david.brad@goodlifefitness.com>
2019-12-04 16:07:29 -0500

    day 4 wip

---
+++ Day04/part01.js
@@ -0,0 +1,14 @@
+// parse
+// convert to int
+// increment
+// convert to array?
+// compare pairs for equal or a<b
+// fail if a>b
+// set doubles to true if equal
+function check(password) {
+  const digits = [];
+}
+
+function checkRange(rangeInput) {}
+
+export function main(input) {}
